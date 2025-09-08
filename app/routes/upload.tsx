8358a819cd5c382f5f7f4567d2f927b3d2bfa370
ana.medvedeva <anamedvedeva1@gmail.com>
2025-09-08 15:47:21 -0400

    Pushing pdfToImage upload - bug with upload currently

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -1,9 +1,15 @@
 import {type FormEvent, useState} from "react";
 import Navbar from "~/components/Navbar";
 import FileUploader from "~/components/FileUploader";
+import {usePuterStore} from "~/lib/puter";
+import {useNavigate} from "react-router";
+import {convertPdfToImage} from "~/lib/pdfToImage";
+import {generateUUID} from "~/lib/utils";
+import {prepareInstructions} from "../../constants";
 
 const Upload = () => {
-    // Check if upload is processing
+    const { auth, isLoading, fs, ai, kv } = usePuterStore();
+    const navigate = useNavigate();
     const [isProcessing, setIsProcessing] = useState(false);
     const [statusText, setStatusText] = useState("");
     const [file, setFile] = useState<File | null>(null);
@@ -12,36 +18,91 @@ const Upload = () => {
         setFile(file);
     }
 
+    const handleAnalyze = async ({ companyName, jobTitle, jobDescription, file}: { companyName:string, jobTitle: string, jobDescription: string, file: File}) => {
+        // Show loading animation
+        setIsProcessing(true);
+
+        setStatusText(`Uploading the file...`);
+
+        // Upload file to Puter storage
+        const uploadedFile = await fs.upload([file]);
+
+        // Check if upload failed, return either success or failed text
+        if(!uploadedFile) return setStatusText(`Error! Upload failed.`);
+        setStatusText('Success! Converting to image...');
+
+        const imageFile = await convertPdfToImage(file);
+        if(!imageFile.file) return setStatusText(`Error! Failed to convert PDF to image.`);
+
+        setStatusText('Uploading the image...');
+
+        const uploadedImage = await fs.upload([imageFile.file]);
+        if(!uploadedFile) return setStatusText(`Error! Failed to upload image.`);
+
+        setStatusText('Preparing data...');
+
+        // Data
+        const uuid = generateUUID();
+        const data = {
+            id: uuid,
+            resumePath: uploadedFile.path,
+            imagePath: uploadedImage.path,
+            companyName, jobTitle, jobDescription,
+            feedback: ''
+        }
+        await kv.set(`resume:${uuid}`, JSON.stringify(data));
+        setStatusText(`Analyzing...`);
+
+        // Generate AI feedback
+        const feedback = await ai.feedback(
+            uploadedFile.path,
+            prepareInstructions({AIResponseFormat: "", jobTitle, jobDescription})
+        )
+
+        if (!feedback) return setStatusText(`Error! Failed to analyze resume.`);
+
+        //Extract feedback
+        const feedbackText = typeof feedback.message.content === 'string' ? feedback.message.content : feedback.message.content[0].text;
+
+        data.feedback = JSON.parse(feedbackText);
+        await kv.set(`resume:${uuid}`, JSON.stringify(data));
+        setStatusText(`Analysis Complete, redirecting...`);
+        console.log(data);
+        navigate(`/resume/${uuid}`);
+    }
+
     const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        const form = e.currentTarget.closest("form");
+        const form = e.currentTarget.closest('form');
         if(!form) return;
         const formData = new FormData(form);
 
-        const companyName = formData.get("company-name");
-        const jobTitle = formData.get("job-title");
-        const jobDescription = formData.get("job-description");
+        const companyName = formData.get('company-name') as string;
+        const jobTitle = formData.get('job-title') as string;
+        const jobDescription = formData.get('job-description') as string;
 
-        console.log({
-            companyName, jobTitle, jobDescription, file
-        })
+        if(!file) return;
+
+        handleAnalyze({ companyName, jobTitle, jobDescription, file });
+
     }
+
     return (
-        <main className="bg-[url('./images/bg-main.svg')] bg-cover">
+        <main className="bg-[url('/images/bg-main.svg')] bg-cover">
             <Navbar />
+
             <section className="main-section">
                 <div className="page-heading py-16">
                     <h1>Smart feedback for your dream job</h1>
                     {/*If processing, render upload gif or prompt to drop your resume*/}
                     {isProcessing ? (
                         <>
-                        <h2>{statusText}</h2>
+                            <h2>{statusText}</h2>
                             <img src="/images/resume-scan.gif" className="w-full" alt="scan gif" />
                         </>
                     ) : (
                         <h2>Drop your resume for an ATS score and improvement tips</h2>
-                    )
-                    }
+                    )}
 
                     {/*If not processing, render form*/}
                     {!isProcessing && (
@@ -57,7 +118,7 @@ const Upload = () => {
                             </div>
 
                             <div className="form-div">
-                                <label htmlFor="job-desription">Job Description</label>
+                                <label htmlFor="job-description">Job Description</label>
                                 <textarea rows={5} name="job-description" id="job-description" placeholder="Job Description" />
                             </div>
 
